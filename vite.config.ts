7ddeba55535250e21b15349af312e8d294669af8
GuDoYoon <rhehfl418q@gmail.com>
2025-06-01 16:25:00 +0900

    (#1): swc  의존성  추가, pnpm 의존성 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),

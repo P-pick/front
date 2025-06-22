f033b8824c5f9ebf79a8d16481057995bb979e1c
GuDoYoon <rhehfl418q@gmail.com>
2025-06-22 14:57:08 +0900

    (hotfix): 로컬 https 적용

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,18 @@ import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
+  plugins: [react(), tailwindcss(), svgr(), basicSsl()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
   server: {
+    host: '0.0.0.0',
     proxy: {
       '/api': {
         target: 'https://apis.data.go.kr/B551011/KorService2',

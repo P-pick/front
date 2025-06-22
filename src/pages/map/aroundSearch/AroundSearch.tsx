2d01979b1f4e39978ffa1870c1cd34135866011e
zzzRYT <jinjinstar3@gmail.com>
2025-06-22 17:56:18 +0900

    (#37): 로딩 적용 안된거 적용

--- src/pages/map/aroundSearch/AroundSearch.tsx
+++ src/pages/map/aroundSearch/AroundSearch.tsx
@@ -9,14 +9,6 @@ const destination = {
   lng: 127.095991,
 };
 
-function ChangeLoadingStatusOnMap() {
-  return (
-    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-24 h-24 z-(--z-layer3) flex items-center justify-center">
-      <LoadingSpinner />
-    </div>
-  );
-}
-
 export default function AroundSearch() {
   const { geoLocation } = useCurrentLocation();
 
@@ -24,12 +16,12 @@ export default function AroundSearch() {
     !isValidationLocation(geoLocation) ||
     !isValidationLocation(destination)
   ) {
-    return <ChangeLoadingStatusOnMap />;
+    return <LoadingSpinner centered={true} />;
   }
 
   return (
     <>
-      <Suspense fallback={<ChangeLoadingStatusOnMap />}>
+      <Suspense fallback={<LoadingSpinner centered={true} />}>
         <GeoAroundTouristMap />
       </Suspense>
     </>

bc17811e9b7d69777763c91d98ef7fbbf6d2d36f
GuDoYoon <rhehfl418q@gmail.com>
2025-06-29 16:25:00 +0900

    (#45): TouristContentsTypeFilter 컴포넌트 추가

--- src/pages/tour/tourList/TourList.tsx
+++ src/pages/tour/tourList/TourList.tsx
@@ -1,13 +1,17 @@
+import { TouristContentsTypeFilter } from '@/components';
 import { SkeletonCard } from './components';
 import TourListContainer from '@/pages/tour/tourList/components/TourListContainer';
 import { Suspense } from 'react';
+import Header from '@/components/Header';
 const fallbackList = [1, 2, 3, 4, 5];
 
 export default function TourList() {
   return (
     <div className="absolute h-full w-full overflow-auto ">
-      <h1 className="absolute right-1/2 translate-x-1/2 top-5">리스트</h1>
-      <div className="mt-15">선택리스트</div>
+      <Header className="w-full flex items-center justify-between px-5 mt-1.5">
+        <h1>리스트</h1>
+      </Header>
+      <TouristContentsTypeFilter />
       <Suspense
         fallback={fallbackList.map(v => (
           <div key={v} className="my-1">

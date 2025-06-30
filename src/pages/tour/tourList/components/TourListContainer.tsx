230e2c7cbef734c2507f6719bb410d922f69eb5b
GuDoYoon <rhehfl418q@gmail.com>
2025-06-30 15:04:17 +0900

    (#45): 캐싱 데이터 로직 추가

--- src/pages/tour/tourList/components/TourListContainer.tsx
+++ src/pages/tour/tourList/components/TourListContainer.tsx
@@ -1,24 +1,22 @@
 import { withGeoTripParams } from '@/pages/tour/components';
 import { useGeoLocationBasedTourQuery } from '../../service';
-import type { GeoTripLocation, TourItemWithDetail } from '@/pages/types';
+import type { GeoTripLocation } from '@/pages/types';
 import { InfiniteScroll, SkeletonCard, TourInfoCard } from '.';
-
 import type { AroundContentTypeId } from '@/pages/map/aroundSearch/types';
-import { useDeferredValue, useEffect } from 'react';
+import { useDeferredValue } from 'react';
 import { useSyncedState } from '../lib';
-import { QueryClient } from '@tanstack/react-query';
+import { queryClient } from '@/config/QueryProvider';
 interface TourListContainerProps {
   location: GeoTripLocation;
   distance: string;
   tourType: AroundContentTypeId;
 }
+
 function TourListContainer({
   location,
   distance,
   tourType,
 }: TourListContainerProps) {
-  const queryClient = new QueryClient();
-
   const [localTourType] = useSyncedState(tourType);
   const deferredTourType = useDeferredValue(localTourType);
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
@@ -27,17 +25,20 @@ function TourListContainer({
       radius: distance,
       contentTypeId: deferredTourType,
     });
-  const cacheData = queryClient.getQueryState<TourItemWithDetail[]>([
+  const cachedData = queryClient.getQueryData([
     'locationBasedData',
     {
       location,
       radius: distance,
-      contentTypeId: deferredTourType,
+      contentTypeId: localTourType,
     },
   ]);
 
   const tourItems = data.pages.flatMap(page => page.items);
-  const isPending = localTourType !== deferredTourType || cacheData?.data;
+
+  const hasCache = Boolean(cachedData);
+  const isSwitching = localTourType !== deferredTourType;
+  const showFallback = isSwitching && !hasCache;
 
   return (
     <>
@@ -45,7 +46,7 @@ function TourListContainer({
         {tourItems.map(tourInfo => (
           <TourInfoCard tourInfo={tourInfo} key={tourInfo.contentid} />
         ))}
-        {isPending && (
+        {showFallback && (
           <div className="absolute inset-0 bg-primary-gray/40 z-10" />
         )}
       </section>

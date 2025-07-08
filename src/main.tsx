08cd7d4f6ea32f727103ccfe3d26a2feb10490ea
zzzRYT <jinjinstar3@gmail.com>
2025-07-08 17:49:56 +0900

    (#64): main.tsx수정

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 
-const KAKAO_MAP_KEY = import.meta.env.VITE_KAKAO_MAP_KEY;
-if (KAKAO_MAP_KEY) {
-  const script = document.createElement('script');
-  script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${KAKAO_MAP_KEY}&libraries=services,clusterer&autoload=false`;
-  script.async = true;
-  document.head.appendChild(script);
-} else {
-  console.error(
-    'KAKAO_MAP_KEY is not defined. Please set it in your environment variables.',
-  );
-}
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <div id="app-wrapper">

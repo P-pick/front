ba9b2e4f726541a5d75244ea7cbbc31db0f68a20
GuDoYoon <rhehfl418q@gmail.com>
2025-07-19 01:27:42 +0900

    (#81): 미사용 커스텀 훅 제거

--- src/pages/tour/geotrip/lib/index.ts
+++ src/pages/tour/geotrip/lib/index.ts
@@ -2,4 +2,3 @@ export { default as persistSlideSession } from './persistSlideSession';
 export { default as useInfiniteSwiperControl } from './useInfiniteSwiperControl';
 export { default as useStartTrip } from './useStartTrip';
 export { default as useTTS } from './useTTS';
-export { default as useUnmount } from './useUnmount';

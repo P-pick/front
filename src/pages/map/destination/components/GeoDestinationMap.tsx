fc7dde9b14ba9ada01bb0e3d259b8493f8651d4b
zzzRYT <jinjinstar3@gmail.com>
2025-07-20 14:24:14 +0900

    (#79): 자전거 클릭시, 카카오 지도로 이동될 수 있게끔 버튼 추가

--- src/pages/map/destination/components/GeoDestinationMap.tsx
+++ src/pages/map/destination/components/GeoDestinationMap.tsx
@@ -6,6 +6,7 @@ import PublicTransit from './publicTransit/PublicTransit';
 import { Pedestrian } from './pedestrian';
 import { Car } from './car';
 import { LoadingSpinner } from '@/components';
+import handleRedirectTransportation from '../service/redirectTransportation';
 
 interface GeoDestinationMapProps {
   start: GeoTripLocation;
@@ -26,6 +27,18 @@ function GeoDestinationMap({ start, end }: GeoDestinationMapProps) {
   if (vehicle === 'publictransit') {
     return <PublicTransit start={start} end={end} />;
   }
+
+  if (vehicle === 'bicycle') {
+    return (
+      <div
+        className="absolute left-0 top-24 z-(--z-layer2) cursor-pointer text-center text-sm text-gray-500 hover:text-gray-700"
+        onClick={() => handleRedirectTransportation({ start, end, vehicle })}
+      >
+        개발중... [카카오 지도로 이동하기]
+      </div>
+    );
+  }
+
   return (
     <>
       <LoadingSpinner centered={true} />
--- src/pages/map/destination/service/index.ts
+++ src/pages/map/destination/service/index.ts
@@ -1,3 +1,4 @@
-export { default as selectedTransportation } from './getTransportationSelected';
-export { default as getCarDestinationQueryOptions} from'./getCarData';
-export { default as getPedestrianDestinationQueryOptions} from './getPedestrianData';
+export { default as getCarDestinationQueryOptions } from './getCarData';
+export { default as getPedestrianDestinationQueryOptions } from './getPedestrianData';
+export { default as getTransitDestinationQueryOptions } from './getTransitData';
+export { default as getBicycleDestination } from './redirectTransportation';

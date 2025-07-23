fd5c81c9c62e21a154c2d4da0e652bf6281077d7
GuDoYoon <rhehfl418q@gmail.com>
2025-07-23 16:26:11 +0900

    fix: rename SideButtonGroup -> sideButtonGroup

--- src/features/tour/ui/SideButtonGroup/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as SideButtonGroupContainer } from './SideButtonGroupContainer';
-export { default as PlayTTSButton } from './PlayTTSButton';
-export { default as GoToAroundTouristButton } from './GoToAroundTouristButton';

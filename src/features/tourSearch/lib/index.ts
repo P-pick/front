0489eebd2357f4d5c16a23e5f44ff87392a93c14
GuDoYoon <rhehfl418q@gmail.com>
2025-07-28 01:30:50 +0900

    (#91): 거리 계산 함수 작성

--- src/features/tourSearch/lib/index.ts
+++ src/features/tourSearch/lib/index.ts
@@ -1,1 +1,2 @@
 export * from './highlightKeyword';
+export * from './getDistanceFromLatLonInMeters';
--- src/features/tourSearch/ui/SearchResult.tsx
+++ src/features/tourSearch/ui/SearchResult.tsx
@@ -1,8 +1,9 @@
 import { useSuspenseInfiniteQuery } from '@tanstack/react-query';
 
+import { getDistanceFromLatLonInMeters } from '@/features/tourSearch';
 import { TourInfoCard } from '@/features/tourList';
 import { tourQueries } from '@/entities/tour';
-import { InfiniteScroll } from '@/shared';
+import { getSuspenseLocation, InfiniteScroll } from '@/shared';
 
 interface SearchResultProps {
   keyword: string;
@@ -17,15 +18,29 @@ export default function SearchResult({ keyword }: SearchResultProps) {
   } = useSuspenseInfiniteQuery(tourQueries.searchKeyWord(keyword));
   const filterSearchData = searchData.pages.filter(data => data.items);
   const flatSearchData = filterSearchData.flatMap(page => page.items.item);
+  const location = getSuspenseLocation();
 
   return (
     <>
       <ul>
-        {flatSearchData.map(item => (
-          <li key={item.contentid}>
-            <TourInfoCard tourInfo={item} />
-          </li>
-        ))}
+        {flatSearchData.map(item => {
+          const newItem = {
+            ...item,
+            dist: String(
+              getDistanceFromLatLonInMeters(
+                item.mapx,
+                item.mapy,
+                location.lat,
+                location.lng,
+              ),
+            ),
+          };
+          return (
+            <li key={item.contentid}>
+              <TourInfoCard tourInfo={newItem} />
+            </li>
+          );
+        })}
       </ul>
       <InfiniteScroll
         hasNextPage={hasNextPage}

6ad14420040f707c13677f72bf0df2bdc0adb4f6
GuDoYoon <rhehfl418q@gmail.com>
2025-07-27 15:49:42 +0900

    (#91): 검색어 state 제거

--- src/features/tourSearch/ui/SearchContainer.tsx
+++ src/features/tourSearch/ui/SearchContainer.tsx
@@ -1,4 +1,4 @@
-import { Suspense, useState } from 'react';
+import { Suspense } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import { useSearchParams } from 'react-router-dom';
 
@@ -7,21 +7,19 @@ import { useDebouncedCallback } from '@/shared';
 
 export default function SearchContainer() {
   const [searchParams, setSearchParams] = useSearchParams();
-  const [keyWord, setKeyWord] = useState(searchParams.get('q') ?? '');
+  const keyword = searchParams.get('q') ?? '';
 
-  const handleSetKeyword = (keyword: string) => {
-    setSearchParams(keyword ? { q: keyword } : {});
-    setKeyWord(keyword);
-  };
-  const debouncedSetKeyword = useDebouncedCallback(handleSetKeyword, 300);
+  const debouncedSetKeyword = useDebouncedCallback((kw: string) => {
+    setSearchParams(kw ? { q: kw } : {}, { replace: true });
+  }, 300);
 
   return (
     <>
-      <SearchForm setKeyword={debouncedSetKeyword} defaultValue={keyWord} />
+      <SearchForm setKeyword={debouncedSetKeyword} defaultValue={keyword} />
       <section className="overflow-y-scroll pb-25 h-screen">
         <ErrorBoundary fallback={<div>검색어를 입력해주세요.</div>}>
           <Suspense fallback={<div>검색 결과를 불러오는 중...</div>}>
-            {keyWord !== '' && <SearchResult keyword={keyWord} />}
+            {keyword !== '' && <SearchResult keyword={keyword} />}
           </Suspense>
         </ErrorBoundary>
       </section>

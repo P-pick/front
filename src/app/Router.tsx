ab668399f19e48fbc6e2adcfe942b5ed4e3ad248
GuDoYoon <rhehfl418q@gmail.com>
2025-07-30 17:29:39 +0900

    (#101): lazy import 적용

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -1,30 +1,35 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import { Home } from '@/pages/home';
 import { GeoTrip } from '@/pages/tour/geotrip';
 import { GeoSetup } from '@/pages/geoSetup';
-import { TourList } from '@/pages/tour/tourList';
 import { Tour } from '@/pages/tour';
-import { Map } from '@/pages/map';
-import { Destination } from '@/pages/map/destination';
-import { AroundSearch } from '@/pages/map/aroundSearch';
-import { TourSearch } from '@/pages/tour/search';
-
+const Map = lazy(() => import('@/pages/map/Map'));
+const Destination = lazy(() => import('@/pages/map/destination/Destination'));
+const AroundSearch = lazy(
+  () => import('@/pages/map/aroundSearch/AroundSearch'),
+);
+const TourList = lazy(() => import('@/pages/tour/tourList/TourList'));
+const TourSearch = lazy(() => import('@/pages/tour/search/TourSearch'));
 export default function Router() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/geo-setup" element={<GeoSetup />} />
-        <Route path="/map" element={<Map />}>
-          <Route path="destination" element={<Destination />} />
-          <Route path="around-search" element={<AroundSearch />} />
-        </Route>
-        <Route path="/tour" element={<Tour />}>
-          <Route path="geo-trip" element={<GeoTrip />} />
-          <Route path="list" element={<TourList />} />
-          <Route path="search" element={<TourSearch />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/geo-setup" element={<GeoSetup />} />
+          <Route path="/map" element={<Map />}>
+            <Route path="destination" element={<Destination />} />
+            <Route path="around-search" element={<AroundSearch />} />
+          </Route>
+          <Route path="/tour" element={<Tour />}>
+            <Route path="geo-trip" element={<GeoTrip />} />
+            <Route path="list" element={<TourList />} />
+            <Route path="search" element={<TourSearch />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }

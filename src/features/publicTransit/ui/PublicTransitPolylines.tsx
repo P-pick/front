368cb59317a1f8bcca6b2639f980aa297d0f0d21
zzzRYT <jinjinstar3@gmail.com>
2025-08-13 23:27:01 +0900

    (#114): 대중교통 globalIndex 추가

--- src/features/publicTransit/ui/PublicTransitPolylines.tsx
+++ src/features/publicTransit/ui/PublicTransitPolylines.tsx
@@ -1,13 +1,12 @@
+import { Fragment } from 'react';
 import { useStore } from 'zustand';
 import { MapMarker, Polyline } from 'react-kakao-maps-sdk';
 import {
   getCoordinatesPointLines,
   useMapLevelStore,
 } from '@/features/navigate';
-
 import type { PUBLICTRANSIT } from '@/entities/navigate';
 import { useMapController } from '@/features/map';
-import { Fragment } from 'react';
 
 interface PublicTransitProps {
   itinerary: PUBLICTRANSIT.Itinerary;
@@ -19,163 +18,120 @@ export default function PublicTransitPolylines({
   const { handleGoToFollowPin } = useMapController();
   const { mapLevel } = useStore(useMapLevelStore);
 
-  return itinerary.legs.map((leg, legIndex) => {
-    if (leg.mode === 'WALK') {
-      return (
-        <Fragment key={`public-transit-walk-${legIndex}`}>
-          {leg.steps?.map((step, index) => {
-            const coordinates = step.linestring
-              .split(' ')
-              .map(coord => coord.split(',').map(Number));
-
-            const path = getCoordinatesPointLines(coordinates);
-
-            return (
-              <Fragment key={`public-transit-walk-${legIndex}-${index}`}>
-                <Polyline
-                  path={path}
-                  strokeColor="#999999"
-                  strokeOpacity={0.8}
-                  strokeWeight={5}
-                />
-                {mapLevel < 6 && (
-                  <MapMarker
-                    position={path[0]}
-                    image={{
-                      src: '/pointIcon.svg',
-                      size: { width: 12, height: 12 },
-                      options: {
-                        offset: {
-                          x: 6,
-                          y: 6,
-                        },
-                      },
-                    }}
-                    zIndex={2}
-                    onClick={() => handleGoToFollowPin(path[0], legIndex)}
-                  />
-                )}
-              </Fragment>
-            );
-          })}
-        </Fragment>
-      );
-    }
-
-    if (leg.mode === 'SUBWAY') {
-      if (!leg.passShape) {
-        return null;
+  // startIndices 계산
+  const startIndices = (() => {
+    const arr: number[] = [];
+    let running = 0;
+    for (const leg of itinerary.legs) {
+      arr.push(running);
+      let size = 0;
+      if (leg.mode === 'WALK') {
+        size = leg.steps?.length ?? 0;
+      } else {
+        // SUBWAY/BUS/TRAIN 등은 passStopList.stationList 길이를 사용
+        size = leg.passStopList?.stationList?.length ?? 0;
       }
-      const coordinates = leg.passShape.linestring
-        .split(' ')
-        .map(coord => coord.split(',').map(Number));
-
-      const path = getCoordinatesPointLines(coordinates);
-
-      return (
-        <Fragment key={`public-transit-subway-${leg.routeId}`}>
-          <Polyline
-            path={path}
-            strokeColor={`#${leg.routeColor}`}
-            strokeOpacity={0.8}
-            strokeWeight={5}
-          />
-          {mapLevel < 6 &&
-            leg.passStopList.stationList.map(station => {
-              return (
-                <MapMarker
-                  onClick={() => handleGoToFollowPin(path[0], legIndex)}
-                  key={station.stationID}
-                  position={{
-                    lat: Number(station.lat),
-                    lng: Number(station.lon),
-                  }}
-                  image={{
-                    src: `/publictransit/subway_${leg.type}line.svg`,
-                    size: { width: 24, height: 24 },
-                  }}
-                ></MapMarker>
-              );
-            })}
-        </Fragment>
-      );
+      running += size;
     }
+    return arr;
+  })();
 
-    if (leg.mode === 'BUS') {
-      if (!leg.passShape) {
+  return (
+    <>
+      {itinerary.legs.map((leg, legIndex) => {
+        const startIndex = startIndices[legIndex];
+
+        if (leg.mode === 'WALK') {
+          return (
+            <Fragment key={`public-transit-walk-${legIndex}`}>
+              {leg.steps?.map((step, index) => {
+                const globalIndex = startIndex + index;
+                const coordinates = step.linestring
+                  .split(' ')
+                  .map(coord => coord.split(',').map(Number));
+                const path = getCoordinatesPointLines(coordinates);
+
+                return (
+                  <Fragment key={`walk-${globalIndex}`}>
+                    <Polyline
+                      path={path}
+                      strokeColor="#999999"
+                      strokeOpacity={0.8}
+                      strokeWeight={5}
+                    />
+                    {mapLevel < 6 && (
+                      <MapMarker
+                        position={path[0]}
+                        image={{
+                          src: '/pointIcon.svg',
+                          size: { width: 12, height: 12 },
+                          options: { offset: { x: 6, y: 6 } },
+                        }}
+                        zIndex={2}
+                        onClick={() =>
+                          handleGoToFollowPin(path[0], globalIndex)
+                        }
+                      />
+                    )}
+                  </Fragment>
+                );
+              })}
+            </Fragment>
+          );
+        }
+
+        // SUBWAY / BUS / TRAIN
+        if (
+          leg.mode === 'SUBWAY' ||
+          leg.mode === 'BUS' ||
+          leg.mode === 'TRAIN'
+        ) {
+          if (!leg.passShape) return null;
+
+          const coordinates = leg.passShape.linestring
+            .split(' ')
+            .map(coord => coord.split(',').map(Number));
+          const path = getCoordinatesPointLines(coordinates);
+
+          return (
+            <Fragment
+              key={`public-transit-${leg.mode.toLowerCase()}-${leg.routeId}`}
+            >
+              <Polyline
+                path={path}
+                strokeColor={`#${leg.routeColor}`}
+                strokeOpacity={0.8}
+                strokeWeight={5}
+              />
+              {mapLevel < 6 &&
+                leg.passStopList?.stationList?.map((station, i) => {
+                  const globalIndex = startIndex + i;
+                  return (
+                    <MapMarker
+                      key={`stop-${globalIndex}-${station.stationID}`}
+                      onClick={() => handleGoToFollowPin(path[0], globalIndex)}
+                      position={{
+                        lat: Number(station.lat),
+                        lng: Number(station.lon),
+                      }}
+                      image={{
+                        src:
+                          leg.mode === 'SUBWAY'
+                            ? `/publictransit/subway_${leg.type}line.svg`
+                            : leg.mode === 'BUS'
+                              ? `/publictransit/bus_${leg.type}.svg`
+                              : '/pointIcon.svg',
+                        size: { width: 24, height: 24 },
+                      }}
+                    />
+                  );
+                })}
+            </Fragment>
+          );
+        }
+
         return null;
-      }
-      const coordinates = leg.passShape.linestring
-        .split(' ')
-        .map(coord => coord.split(',').map(Number));
-
-      const path = getCoordinatesPointLines(coordinates);
-
-      return (
-        <Fragment key={`public-transit-bus-${leg.routeId}`}>
-          <Polyline
-            path={path}
-            strokeColor={`#${leg.routeColor}`}
-            strokeOpacity={0.8}
-            strokeWeight={5}
-          />
-          {mapLevel < 6 &&
-            leg.passStopList.stationList.map(stop => {
-              return (
-                <MapMarker
-                  onClick={() => handleGoToFollowPin(path[0], legIndex)}
-                  key={stop.stationID}
-                  position={{
-                    lat: Number(stop.lat),
-                    lng: Number(stop.lon),
-                  }}
-                  image={{
-                    src: `/publictransit/bus_${leg.type}.svg`,
-                    size: { width: 24, height: 24 },
-                  }}
-                ></MapMarker>
-              );
-            })}
-        </Fragment>
-      );
-    }
-
-    if (leg.mode === 'TRAIN') {
-      if (!leg.passShape) {
-        return null;
-      }
-      const coordinates = leg.passShape.linestring
-        .split(' ')
-        .map(coord => coord.split(',').map(Number));
-      const path = getCoordinatesPointLines(coordinates);
-
-      return (
-        <Fragment key={`public-transit-train-${leg.routeId}`}>
-          <Polyline
-            path={path}
-            strokeColor={`#${leg.routeColor}`}
-            strokeOpacity={0.8}
-            strokeWeight={5}
-          />
-          {mapLevel < 6 &&
-            leg.passStopList.stationList.map(station => {
-              return (
-                <MapMarker
-                  onClick={() => handleGoToFollowPin(path[0], legIndex)}
-                  key={station.stationID}
-                  position={{
-                    lat: Number(station.lat),
-                    lng: Number(station.lon),
-                  }}
-                  image={{
-                    src: '/pointIcon.svg',
-                    size: { width: 24, height: 24 },
-                  }}
-                ></MapMarker>
-              );
-            })}
-        </Fragment>
-      );
-    }
-  });
+      })}
+    </>
+  );
 }

5e655fd7f302598dad3ed46f842368286abe7d33
GuDoYoon <rhehfl418q@gmail.com>
2025-09-01 21:37:04 +0900

    (#141):거리 추가

--- src/features/bookmark/ui/BookmarkCard.tsx
+++ src/features/bookmark/ui/BookmarkCard.tsx
@@ -1,6 +1,8 @@
 import { tourQueries } from '@/entities/tour';
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { TourInfoCard } from '@/features/tourList';
+import { getDistanceFromLatLonInMeters } from '@/features/tourSearch';
+import { getSuspenseLocation } from '@/shared/';
 
 interface BookmarkCardProps {
   contentId: string;
@@ -10,10 +12,23 @@ export default function BookmarkCard({ contentId }: BookmarkCardProps) {
   const { data: tourCommon } = useSuspenseQuery(
     tourQueries.detailCommon(contentId),
   );
+  const location = getSuspenseLocation();
 
   return (
     <li className="w-full">
-      <TourInfoCard tourInfo={{ ...tourCommon, dist: '123' }} />
+      <TourInfoCard
+        tourInfo={{
+          ...tourCommon,
+          dist: String(
+            getDistanceFromLatLonInMeters(
+              tourCommon.mapy,
+              tourCommon.mapx,
+              location.lat,
+              location.lng,
+            ),
+          ),
+        }}
+      />
     </li>
   );
 }
--- src/pages/tour/bookmark/Bookmark.tsx
+++ src/pages/tour/bookmark/Bookmark.tsx
@@ -2,6 +2,7 @@ import { authOptions } from '@/entities/auth';
 import { bookmarkOptions } from '@/entities/bookmark';
 import { BookmarkCard } from '@/features/bookmark';
 import { BottomNavigationBar } from '@/shared';
+import { Header } from '@/widgets';
 import { useSuspenseQuery } from '@tanstack/react-query';
 
 export default function Bookmark() {
@@ -12,6 +13,9 @@ export default function Bookmark() {
 
   return (
     <section className="flex max-w-[375px] flex-col h-full">
+      <Header className="w-full flex items-center justify-between px-3 pt-1">
+        <span>북마크</span>
+      </Header>
       <ul className="relative w-full h-full overflow-auto">
         {bookmarkList?.map(contentId => (
           <BookmarkCard key={contentId} contentId={contentId} />

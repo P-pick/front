ec5043e4613216da088d6df4c9afaaea5e3f25d0
zzzRYT <jinjinstar3@gmail.com>
2025-09-01 16:42:37 +0900

    (#140): ErrorFallback위치 변경

--- src/shared/config/index.ts
+++ src/shared/config/index.ts
@@ -1,4 +1,3 @@
 export * from './axios';
 export * from './firebaseConfig';
 export { default as QueryErrorBoundary } from './QueryErrorBoundary';
-export { default as ErrorFallback } from './ErrorFallback';

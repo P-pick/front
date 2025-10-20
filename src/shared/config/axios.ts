766f022858b93a70631a7f66f3fbe5b54bd6bd2b
zzzryt <jinjinstar3@gmail.com>
2025-10-21 00:23:41 +0900

    bugfix(#173): proxy서버에 연결

--- src/shared/config/axios.ts
+++ src/shared/config/axios.ts
@@ -6,12 +6,15 @@ const isLocal = import.meta.env.DEV;
  * TourAPI 요청을 위한 Axios 인스턴스
  */
 export const tourApi = axios.create({
-  baseURL: isLocal ? '/api' : 'https://apis.data.go.kr/B551011/KorService2',
+  baseURL: import.meta.env.VITE_TOUR_BASE_URL,
   params: {
     MobileOS: 'WEB',
     MobileApp: 'p_pick',
     serviceKey: import.meta.env.VITE_TOUR_SERVICE_KEY,
     _type: 'json',
+  },
+  headers: {
+    'ngrok-skip-browser-warning': 'true',
   },
   timeout: 5000,
 });
